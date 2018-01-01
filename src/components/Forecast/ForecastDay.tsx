036b232b1c6021d051719de62df4e58eebc0707f
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 14:42:40 -0500

    Added WeatherIcon to forecast day

--- src/components/Forecast/ForecastDay.tsx
+++ src/components/Forecast/ForecastDay.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 
 import './ForecastDay.styles.css'
 
-const icon = require('../../icons/png/rain-2.png')
-
+// const icon = require('../../icons/png/rain-2.png')
+import WeatherIcon from '../WeatherIcon/WeatherIcon'
 interface ForecastDayProps {
   day: string
   unit: 'C' | 'F'
@@ -14,9 +14,7 @@ const ForecastDay = (props: ForecastDayProps) => {
   return (
     <div className='forecast--day'>
       <p className='forecast--day--name'>{day}</p>
-      <div className='forecast--day--icon'>
-        <img src={icon}/>
-      </div>
+      <WeatherIcon size='small' name='windy'/>
       <p className='forecast--day--hi-temp'>8&deg;{unit}</p>
       <p className='forecast--day--lo-temp'>8&deg;{unit}</p>
     </div>
