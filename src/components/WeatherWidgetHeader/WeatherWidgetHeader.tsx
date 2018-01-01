25a852348a2b1ac7085f6da162f3495be733c861
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 13:16:24 -0500

    Added unit prop

--- src/components/WeatherWidgetHeader/WeatherWidgetHeader.tsx
+++ src/components/WeatherWidgetHeader/WeatherWidgetHeader.tsx
@@ -4,15 +4,16 @@ import './WeatherWidgetHeader.styles.css'
 
 interface WeatherWidgetHeaderProps {
   updated: string
+  unit: 'C' | 'F'
 }
 
 const WeatherWidgetHeader = (props: WeatherWidgetHeaderProps) => {
-  const {updated} = props
+  const { updated, unit } = props
   return (
     <div className='weather-widget__header'>
         <p className='weather-widget__header--icon'>L</p>
         <p className='weather-widget__header--info'>Weather * Updated {updated}</p>
-      <button className='weather-widget__header--button'>+</button>
+      <button className='weather-widget__header--button'>{unit}</button>
     </div>
   )
 }
