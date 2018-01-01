822b5303d5498d0df7c5f0660cdf2f7d4816c0ac
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 03:16:22 -0500

    Added markup for forecast day

--- src/components/WeatherWidget/ForecastDay.tsx
+++ src/components/WeatherWidget/ForecastDay.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react'
 
-const ForecastDay = () => {
+interface ForecastDayProps {
+  day: string
+  unit: 'C' | 'F'
+}
 
+const ForecastDay = (props: ForecastDayProps) => {
+  const { day, unit } = props
+  return (
+    <div className='forecast--day'>
+      <p className='forecast--day--name'>{day}</p>
+      <div className='forecast--day--icon'>I</div>
+      <p className='forecast--day--hi-temp'>8&deg;{unit}</p>
+      <p className='forecast--day--lo-temp'>8&deg;{unit}</p>
+    </div>
+  )
 }
 
 export default ForecastDay
