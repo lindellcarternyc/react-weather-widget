520b5f7359fb3167cc856e08b3580881507495f8
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 01:39:00 -0500

    Added current weather wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,15 @@ class App extends React.Component {
               <p className='weather-widget__header--info'>Title</p>
             <button className='weather-widget__header--button'>+</button>
           </div>
+          <div className='current-weather'>
+            <div className='current-weather--text'>
+              <p>8 &deg; F in New York</p>
+              <p>Clear</p>
+            </div>
+            <div className='current-weather__icon--wrapper'>
+              Icon
+            </div>
+          </div>
         </div>
       </div>
     )
