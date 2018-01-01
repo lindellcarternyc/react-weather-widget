4ad8ceb9253e51a917eacf58764e53409fa54c74
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 01:05:22 -0500

    Added header wirefram

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        Hey There
+        <div className='weather-widget'>
+          <div className='weather-widget__header'>
+              <p className='weather-widget__header--icon'>L</p>
+              <p className='weather-widget__header--info'>Title</p>
+            <button className='weather-widget__header--button'>+</button>
+          </div>
+        </div>
       </div>
     )
   }
