c27ac27c11c7e5aa5c3093850920e71fba0f6c23
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 02:20:45 -0500

    Added wirefram to Weather widget

--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -2,7 +2,25 @@ import * as React from 'react'
 
 const WeatherWidget = () => {
   return (
-    <div>'Weather widget'</div>
+    <div className='weather-widget'>
+          <div className='weather-widget__header'>
+              <p className='weather-widget__header--icon'>L</p>
+              <p className='weather-widget__header--info'>Title</p>
+            <button className='weather-widget__header--button'>+</button>
+          </div>
+          <div className='current-weather'>
+            <div className='current-weather--text'>
+              <p>8 &deg; F in New York</p>
+              <p>Clear</p>
+            </div>
+            <div className='current-weather__icon--wrapper'>
+              Icon
+            </div>
+          </div>
+          <div className='forecast'>
+            {}
+          </div>
+        </div>
   )
 }
 
