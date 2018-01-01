8c92a7bba6d1c292b96d5fa18019036719eccc99
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 14:18:44 -0500

    added summy icons to forecast days

--- src/components/Forecast/ForecastDay.tsx
+++ src/components/Forecast/ForecastDay.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 
 import './ForecastDay.styles.css'
 
+const icon = require('../../icons/png/rain-2.png')
+
 interface ForecastDayProps {
   day: string
   unit: 'C' | 'F'
@@ -12,7 +14,9 @@ const ForecastDay = (props: ForecastDayProps) => {
   return (
     <div className='forecast--day'>
       <p className='forecast--day--name'>{day}</p>
-      <div className='forecast--day--icon'>I</div>
+      <div className='forecast--day--icon'>
+        <img src={icon}/>
+      </div>
       <p className='forecast--day--hi-temp'>8&deg;{unit}</p>
       <p className='forecast--day--lo-temp'>8&deg;{unit}</p>
     </div>
