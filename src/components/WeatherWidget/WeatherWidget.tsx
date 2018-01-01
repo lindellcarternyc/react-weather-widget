3971f09933350892973b90b71f2ed172fe4ccf3f
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-01 02:44:05 -0500

    Added unit prop

--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -2,20 +2,21 @@ import * as React from 'react'
 
 interface WeatherWidgetProps {
   weatherdata: {
+    unit: 'C' | 'F'
     forecastdata: {day: string}[]
   }
 }
 
 const WeatherWidget = (props: WeatherWidgetProps) => {
   const { weatherdata } = props
-  const { forecastdata } = weatherdata
+  const { forecastdata, unit } = weatherdata
   const days = forecastdata.map(data => {
     return (
       <div key={data.day} className='forecast--day'>
         <p className='forecast--day--name'>{data.day}</p>
         <div className='forecast--day--icon'>I</div>
-        <p className='forecast--day--hi-temp'>8&deg;F</p>
-        <p className='forecast--day--lo-temp'>8&deg;F</p>
+        <p className='forecast--day--hi-temp'>8&deg;{unit}</p>
+        <p className='forecast--day--lo-temp'>8&deg;{unit}</p>
       </div>
     )
   })
@@ -24,12 +25,12 @@ const WeatherWidget = (props: WeatherWidgetProps) => {
     <div className='weather-widget'>
           <div className='weather-widget__header'>
               <p className='weather-widget__header--icon'>L</p>
-              <p className='weather-widget__header--info'>Title</p>
+              <p className='weather-widget__header--info'>Weather * Updated 5 minutes ago</p>
             <button className='weather-widget__header--button'>+</button>
           </div>
           <div className='current-weather'>
             <div className='current-weather--text'>
-              <p>8 &deg; F in New York</p>
+              <p>8&deg;{unit} in New York</p>
               <p>Clear</p>
             </div>
             <div className='current-weather__icon--wrapper'>
