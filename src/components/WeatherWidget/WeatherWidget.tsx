419db9ccbab8c4abc327f0b7268ad6ada755889c
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-02 09:07:55 -0500

    Updated to usel models and moment

--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -1,36 +1,31 @@
 import * as React from 'react'
-import * as moment from 'moment'
 
 import WeatherWidgetHeader from '../WeatherWidgetHeader/WeatherWidgetHeader'
 import CurrentWeather from '../CurrentWeather/CurrentWeather'
 import Forecast from '../Forecast/Forecast'
 
-import * as weatherService from '../../weather-service'
+import * as models from '../../models'
+
+import * as moment from 'moment'
 
 import './WeatherWidget.styles.css'
 
 interface WeatherWidgetProps {
-  weatherdata: {
-    updated: moment.Moment
-    unit: 'C' | 'F'
-    forecastdata: {day: string}[]
-  }
-  dt: string
+  updated: moment.Moment
   city: string
-  forecast?: weatherService.Forecast
+  unit: 'C' | 'F'
+  forecast?: models.Forecast
 }
 
 const WeatherWidget = (props: WeatherWidgetProps) => {
-  const { weatherdata, dt, city, forecast } = props
-  const { unit } = weatherdata
-  // const updatedStr = updatedAt.format()
+  const { updated, city, forecast, unit } = props
 
   return (
     <div className='weather-widget'>
-      <WeatherWidgetHeader updated={dt} unit={unit}/>
+      <WeatherWidgetHeader updated={updated} unit={unit}/>
       <CurrentWeather unit={unit} city={city} />
       {forecast && 
-        <Forecast {...weatherdata} forecast={forecast} />
+        <Forecast unit={unit} forecast={forecast} />
       }
     </div>
   )
