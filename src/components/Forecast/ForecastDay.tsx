7ae7e5beb48a8db8d1c6d62d5a49b0d5656bf40a
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-02 09:08:55 -0500

    Refactored to use models

--- src/components/Forecast/ForecastDay.tsx
+++ src/components/Forecast/ForecastDay.tsx
@@ -2,20 +2,21 @@ import * as React from 'react'
 
 import './ForecastDay.styles.css'
 
-import * as weatherService from '../../weather-service'
+// import * as weatherService from '../../weather-service'
+import * as models from '../../models'
 import WeatherIcon from '../WeatherIcon/WeatherIcon'
 interface ForecastDayProps {
-  day: string
   unit: 'C' | 'F'
-  forecastday: weatherService.ForecastDay
+  forecastday: models.ForecastDay
 }
 
 const ForecastDay = (props: ForecastDayProps) => {
-  const { day, unit, forecastday} = props
-  const { high, low } = forecastday 
+  const { unit, forecastday} = props
+  const { name, data } = forecastday
+  const { high, low } = data
   return (
     <div className='forecast--day'>
-      <p className='forecast--day--name'>{day}</p>
+      <p className='forecast--day--name'>{name}</p>
       <WeatherIcon size='small' name='windy'/>
       <p className='forecast--day--hi-temp'>{high.toFixed(0)}&deg;{unit}</p>
       <p className='forecast--day--lo-temp'>{low.toFixed(0)}&deg;{unit}</p>
