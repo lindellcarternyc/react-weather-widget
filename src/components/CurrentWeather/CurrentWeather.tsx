cdef27189cd608e33626200f1560acd0f38b4926
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-02 20:31:27 -0500

    Added data prop

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -4,19 +4,28 @@ import WeatherIcon, { IconSize } from '../WeatherIcon/WeatherIcon'
 
 import './CurrentWeather.styles.css'
 
-import { Unit } from '../../models'
+import { Unit, CurrentWeatherData, Temperature } from '../../models'
 
 interface CurrentWeatherProps {
   unit: Unit
   city: string
+  data?: CurrentWeatherData
 }
 
 const CurrentWeather = (props: CurrentWeatherProps) => {
-  const { unit, city } = props
+  const { unit, city, data } = props
+
+  let temperature: Temperature | null
+  if (data !== undefined) {
+    temperature = data.temp
+  } else {
+    temperature = null
+  }
+
   return (
     <div className='current-weather'>
       <div className='current-weather--text'>
-        <p>8&deg;{unit} in {city}</p>
+        <p>{temperature && temperature.toFixed()}&deg;{unit} in {city}</p>
         <p>Clear</p>
       </div>
       <WeatherIcon size={IconSize.large} description=''/>
